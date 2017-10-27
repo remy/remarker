02f0a9e69cdc0c83eaba0c280458f807aa7bf778
Remy Sharp <remy@remysharp.com>
2017-10-27 14:26:50 +0100

    feat: add live reload by default
    and add external CSS support

--- index.js
+++ index.js
@@ -1,54 +1,65 @@
 #!/usr/bin/env node
 
-const { asset, dest, name, on, port } = require('berber')
-const layout1 = require('layout1')
-const rename = require('gulp-rename')
-const { readFileSync } = require('fs')
-const { join } = require('path')
-require('require-yaml')
+const bulbo = require('bulbo');
+const { action, loggerTitle, asset, dest, name, on, port } = require('berber');
+const layout1 = require('layout1');
+const rename = require('gulp-rename');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+require('require-yaml');
+const livereload = require('livereload');
+var liveServer = null;
+const assets = ['.'];
 
-const script = readFileSync(join(__dirname, 'vendor/remark.js'))
-const layoutFilename = join(__dirname, 'layout.njk')
+const live = process.argv.length === 2; // this is crap
 
-const defaultCss = `
-  body {
-    font-family: 'Avenir Next', 'Hiragino Kaku Gothic ProN', 'Meiryo', 'メイリオ', sans-serif;
-  }
-  h1, h2, h3 {
-    font-weight: bold;
-  }
-  .remark-code,
-  .remark-inline-code {
-    font-family: 'Menlo', 'Monaco', 'Courier new', monospace;
-  }
+const script = readFileSync(join(__dirname, 'vendor/remark.js'));
+const defaultCss = readFileSync(join(__dirname, 'vendor/remark.css'));
+const layoutFilename = join(__dirname, 'layout-updated.njk');
 
-  .remark-slide-content.inverse {
-    color: #f3f3f3;
-    background-color: #272822;
-  }
-`
+const cwd = process.cwd();
 
-name('remarker')
+action('serve', () => {
+  liveServer = livereload.createServer({
+    exts: ['md', 'js', 'css'],
+  });
+  liveServer.watch(assets);
+  bulbo.serve();
+});
+
+name('remarker');
+loggerTitle('remarker');
 
 on('config', config => {
-  config = config || {}
+  config = config || {};
 
-  port(config.port || 6275)
-  dest(config.dest || 'build')
+  port(config.port || 6275);
+  dest(config.dest || 'build');
+
+  if (config.stylesheet) {
+    asset(config.stylesheet).base(cwd);
+    assets.push(config.stylesheet);
+  }
+
+  asset(join(__dirname, 'vendor/remark.js')).base(__dirname);
 
   asset(config.source || 'slides.md')
     .pipe(rename({ basename: 'index', extname: '.html' }))
-    .pipe(layout1.nunjucks(layoutFilename, {
-      data: {
-        script,
-        css: config.css || defaultCss,
-        title: config.title || '',
-        remarkConfig: config.remarkConfig || {}
-      }
-    }))
+    .pipe(
+      layout1.nunjucks(layoutFilename, {
+        data: {
+          script,
+          live,
+          css: config.css || defaultCss,
+          title: config.title || '',
+          stylesheet: config.stylesheet || false,
+          remarkConfig: config.remarkConfig || {},
+        },
+      })
+    );
 
-  const assets = config.assets || ['assets']
-  assets.forEach(src => {
-    asset(join(src, '**/*.*')).base(process.cwd())
-  })
-})
+  (config.assets || ['assets']).forEach(src => {
+    assets.push(join(cwd, src));
+    asset(join(src, '**/*.*')).base(cwd);
+  });
+});
